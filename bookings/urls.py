e57f3d3cac4e3453819fa9e2e8dd6f97023dd29f
BlueMogwai <sararojogomez@gmail.com>
2024-04-30 20:45:26 +0200

    added login and clase search

--- bookings/urls.py
+++ bookings/urls.py
@@ -7,16 +7,23 @@ from .views import (
     ClaseDeleteView,
     ClaseUpdateView,
     ClaseCreateView,
+    user_login_view,
+    user_logout_view,
+    UserUpdateView,
+    clase_search_view,
 )
 
 
 urlpatterns = [
-    path("", home_view),
+    path("", home_view, name="home"),
     path("clase/list/", ClaseListView.as_view(), name="clase-list"),
     path("clase/<int:pk>/detail/", ClaseDetailView.as_view(), name="clase-detail"),
     path("clase/<int:pk>/delete/", ClaseDeleteView.as_view(), name="clase-delete"),
     path("clase/<int:pk>/update/", ClaseUpdateView.as_view(), name="clase-update"),
     path("clase/create/", ClaseCreateView.as_view(), name="clase-create"),
-
+    path("clase/buscar/", clase_search_view, name="clase-buscar"),
+    path("login/", user_login_view, name="login"),
+    path("logout/", user_logout_view, name="logout"),
+    path('editar-perfil/', UserUpdateView.as_view(), name='editar-perfil'),
 
 ]
--- bookings/views.py
+++ bookings/views.py
@@ -1,6 +1,7 @@
 from .models import Grupo, Clase, Reserva
-# from .forms import UserEditForm, JuegoSearchForm, SistemaSearchForm
-from django import forms
+from .forms import UserEditForm
+from .forms import ClaseSearchForm
+# from django import forms
 from django.urls import reverse_lazy
 from django.views.generic import(
     ListView,
@@ -14,17 +15,12 @@ from django.contrib.auth import login, logout
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.decorators import login_required
+# from django.contrib.auth.mixins import rom django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def home_view(request):
     return render(request, "bookings/home.html")
-
-# -----------------------------------------------------------------------------
-# CRUD: CLASES
-# -----------------------------------------------------------------------------
-
 
 
 class ClaseListView(LoginRequiredMixin, ListView):
@@ -58,3 +54,49 @@ class ClaseCreateView(LoginRequiredMixin, CreateView):
     template_name = "bookings/vbc/clase_form.html"
     fields = ["nombre", "disponible", "capacidad", "grupo", "descripcion"]
     success_url = reverse_lazy("clase-list")
+
+
+def clase_search_view(request):
+    if request.method == "GET":
+        form = ClaseSearchForm()
+        return render(
+            request, "bookings/form_search.html", context={"search_form": form}
+        )
+    elif request.method == "POST":
+        form = ClaseSearchForm(request.POST)
+        if form.is_valid():
+            nombre_de_clase = form.cleaned_data["nombre"]
+            clases_encontradas = Clase.objects.filter(nombre=nombre_de_clase).all()
+            contexto_dict = {"todas_las_clases": clases_encontradas}
+            return render(request, "bookings/vbc/clase_list.html", contexto_dict)
+        else:
+            return render(
+                request, "bookings/form_search.html", context={"search_form": form}
+            )
+
+def user_login_view(request):
+    if request.method == "GET":
+        form = AuthenticationForm()
+    elif request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            user = form.user_cache
+            if user is not None:
+                login(request, user)
+                return redirect("home")
+
+    return render(request, "bookings/login.html", {"acceder": form})
+
+def user_logout_view(request):
+    logout(request)
+    return redirect("login")
+
+class UserUpdateView(LoginRequiredMixin, UpdateView):
+    model = User
+    form_class = UserEditForm
+    template_name = 'bookings/user_edit_form.html'
+    success_url = reverse_lazy('home')
+
+    def get_object(self):
+        return self.request.user
